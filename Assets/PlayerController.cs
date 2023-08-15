15642eb4ec95b8683108f47e4a5cc27ec4e9b952
perochi <s.shumpei@outlook.jp>
2023-08-15 23:48:55 +0900

    no message

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,39 +5,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    private Vector3 targetPosition;
-    private Animator animator;
+    [SerializeField] float speed;
+    bool isMoving;
 
-    [SerializeField]
-    private Tilemap tilemap;
-
-    void Start()
+    private void Update()
     {
-        animator = GetComponent<Animator>();
-    }
+        if (isMoving)
+        {
+            return;
+        }
 
-    
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+        if (Input.GetMouseButtonDown(0))
+        {
 
-        transform.Translate(movement);
-
-        float speed = movement.magnitude;
-        animator.SetFloat("Speed", speed);
-    }
-
-    public void MoveToGridPosition(Vector3 position)
-    {
-        targetPosition = position;
-    }
-
-    public void MoveToPosition(Vector3 position)
-    {
-        Vector3 complementPos = new Vector3(tilemap.cellSize.x / 2, 0.0f, tilemap.cellSize.y / 2);
-        targetPosition = position + complementPos;
+        }
     }
 }
